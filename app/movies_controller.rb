af3a41e251d499af437f7cb474aac02ba9587c73
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-20 18:10:00 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
   
   Movie.create do |m|
-    movie.name = title
-    movie.release_date = date 
+    m.name = title
+    m.release_date = date 
   end
 end
 
